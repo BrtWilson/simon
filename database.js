1ef5cf2be4bfb23019a53d5a5408452d1fca9b7c
Bryton Scott Wilson <brytonsw@rogers.cs.byu.edu>
2023-03-25 22:49:16 -0600

    fix database.js

--- database.js
+++ database.js
@@ -5,26 +5,26 @@ const password = process.env.MONGOPASSWORD;
 const hostname = process.env.MONGOHOSTNAME;
 
 if (!userName) {
-    throw Error('Database not configured. Set environment variables');
+  throw Error('Database not configured. Set environment variables');
 }
 
-const url = `mongodb+src://${userName}:${password}@${hostname}`;
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
 const scoreCollection = client.db('simon').collection('score');
 
 function addScore(score) {
-    scoreCollection.insertOne(score);
+  scoreCollection.insertOne(score);
 }
 
 function getHighScores() {
-    const query = {score: {$gt: 0}};
-    const options = {
-        sort: {score: -1},
-        limit: 15,
-    };
-    const cursor = scoreCollection.fing(query, options);
-    return cursor.toArray();
+  const query = {score: {$gt: 0}};
+  const options = {
+    sort: {score: -1},
+    limit: 15,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
 }
 
 module.exports = {addScore, getHighScores};
